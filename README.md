6ad5cb70a6c45cf7936729ca04ad0ba0249f9971
rhe24 <rhe24@aber.ac.uk>
2018-11-21 15:15:59 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Language Vocabulary Assistant
+
+## Synopsis
+An app to help users learn vocabulary of a foreign language of their choice.
+
+## Installing
+
+## Deployment
---
+++ app/src/main/java/rhe24/cs31620/dcs/aber/ac/uk/lva/LVAMainActivity.java
@@ -0,0 +1,13 @@
+package rhe24.cs31620.dcs.aber.ac.uk.lva;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LVAMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lvamain);
+    }
+}

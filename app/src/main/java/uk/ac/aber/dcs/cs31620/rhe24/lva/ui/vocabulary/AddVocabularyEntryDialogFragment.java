15440f3ec4ca65aa8e70a3b5ab5c712ad529acd9
rhe24 <rhys301097@gmail.com>
2018-12-01 19:29:58 +0000

    added savedinstance bundle implementation for add new vocab entry dialog

--- app/src/main/java/uk/ac/aber/dcs/cs31620/rhe24/lva/ui/vocabulary/AddVocabularyEntryDialogFragment.java
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/rhe24/lva/ui/vocabulary/AddVocabularyEntryDialogFragment.java
@@ -3,14 +3,10 @@ package uk.ac.aber.dcs.cs31620.rhe24.lva.ui.vocabulary;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.app.Fragment;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.widget.EditText;
 
 import uk.ac.aber.dcs.cs31620.rhe24.lva.R;
 
@@ -22,6 +18,26 @@ import uk.ac.aber.dcs.cs31620.rhe24.lva.R;
  * @version 30/11/2018
  */
 public class AddVocabularyEntryDialogFragment extends DialogFragment {
+
+    /**
+     * The key that the word in the primary language is saved under in saved instance state bundle
+     */
+    private static final String PRIMARY_LANGUAGE_WORD_KEY = "PRIMARY_LANGUAGE_WORD";
+
+    /**
+     * The key that the word in the secondary language is saved under in saved instance state bundle
+     */
+    private static final String SECONDARY_LANGUAGE_WORD_KEY = "SECONDARY_LANGUAGE_WORD";
+
+    /**
+     * The view object of the primary word input
+     */
+    private EditText primaryWordInput;
+
+    /**
+     * The view object of the secondary word input
+     */
+    private EditText secondaryWordInput;
 
     // Empty Constructor
     public AddVocabularyEntryDialogFragment(){
@@ -42,19 +58,51 @@ public class AddVocabularyEntryDialogFragment extends DialogFragment {
      */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // The dialog builder
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.DialogTheme);
-
         // Get the activity's layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        // Build the dialog by inflating view and setting buttons
-        builder.setView(inflater.inflate(R.layout.fragment_add_vocabulary_entry, null));
+        // Create the dialog view by inflating the layout
+        View view = inflater.inflate(R.layout.fragment_add_vocabulary_entry, null);
+
+        // Initialize the word input objects
+        primaryWordInput = view.findViewById(R.id.primary_word_input);
+        secondaryWordInput = view.findViewById(R.id.secondary_word_input);
+
+        // If a savedInstanceState bundle contains the edit text contents, place them in there
+        if(savedInstanceState != null){
+            if(savedInstanceState.containsKey(PRIMARY_LANGUAGE_WORD_KEY)){
+                primaryWordInput.setText(savedInstanceState.getString(PRIMARY_LANGUAGE_WORD_KEY));
+            }
+
+            if(savedInstanceState.containsKey(SECONDARY_LANGUAGE_WORD_KEY)){
+                secondaryWordInput.setText(savedInstanceState.getString(SECONDARY_LANGUAGE_WORD_KEY));
+            }
+        }
+
+
+        // Build the dialog by assigning view
+        builder.setView(view);
 
         // Add buttons
         // TODO: Add behaviour via viewmodel
         builder.setPositiveButton(R.string.dialog_add_word, null);
         builder.setNegativeButton(R.string.dialog_cancel, null);
 
+
         return builder.create();
+    }
+
+    /**
+     * When instance state is saved, add primary word and secondary word to bundle
+     * @param outState
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putString(PRIMARY_LANGUAGE_WORD_KEY, primaryWordInput.getText().toString());
+        outState.putString(SECONDARY_LANGUAGE_WORD_KEY, secondaryWordInput.getText().toString());
     }
 }
